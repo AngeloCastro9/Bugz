cfbca3d1620e2f7887819d2c0d1e5421e7e59195
yuri barros ribeiro <1727469>
2020-02-17 21:21:41 -0300

    Products

---
+++ backend/migrations/20200217210328_products.js
@@ -0,0 +1,18 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('products', table => {
+        table.increments('id').primary()
+        table.string('name').notNull()
+        table.string('description').notNull()
+        table.double('price').notNull()
+        table.integer('restaurantId').references('id')
+            .inTable('restaurants').notNull()
+        table.boolean('vegan').notNull().defaultTo(false)
+        table.integer('rate').defaultTo(0)
+        table.integer('numberOfVotes').defaultTo(0)
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('products')  
+};
