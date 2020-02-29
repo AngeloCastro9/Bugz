e867aaf49c12963d0eaa6d90bf4371c532ba16f2
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-02-28 23:33:57 -0300

    Adicionada consulta para restaurante

---
+++ backend/api/restaurants.js
@@ -0,0 +1,18 @@
+module.exports = app => {
+    const { existsOrError } = app.api.validation
+
+    const limit = 10 // usado para paginação
+    const get = async (req, res) => {
+
+        const result = await app.db('restaurants').count('id').first()
+        const count = parseInt(result.count)
+
+        app.db('retaurants')
+            .select('id', 'name', 'description')
+            .limit(limit)
+            .then(restaurants => res.json({ data: restaurants, count, limit }))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { get, getById }
+}
