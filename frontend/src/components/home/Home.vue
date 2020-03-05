a68726c8597c3b67565a97d3a9971d17e5b7b05d
angelocastro9 <castromatheus060@gmail.com>
2020-03-05 16:10:14 -0300

    Fix card

--- frontend/src/components/home/Home.vue
+++ frontend/src/components/home/Home.vue
@@ -1,15 +1,15 @@
 <template >
   <div class="home">
     <PageTitle icon="fa fa-home" main="Bem vindo!" sub="Bugz" />
-    <b-container>
+    <b-container >
       <b-row>
-        <div v-for="restaurant in restaurants" v-bind:key="restaurant.id">
+        <div v-for="restaurant in restaurants" v-bind:key="restaurant.id" >
           <b-card
             img-src="https://picsum.photos/600/300/?image=25"
             img-alt="Image"
             img-top
             tag="article"
-            style="max-width: 20rem; left: 10%"
+            style="max-width: 20rem; margin: 5%; left: 25%"
             class="mb-3"
           >
             <b-card-text>{{restaurant.name}}</b-card-text>
