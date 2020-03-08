28b4644863d34fcd2d16f71e064880b666f0c87a
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-03-08 12:35:57 -0300

    Criada contagem para vegano e consertada contagem para restaurante

--- backend/api/restaurant.js
+++ backend/api/restaurant.js
@@ -4,7 +4,7 @@ module.exports = app => {
     const limit = 10 // usado para paginação
     const get = async (req, res) => {
 
-        const result = await app.db('users').count('id').first()
+        const result = await app.db('users').where({ is_restaurant: true }).count('id').first()
         const count = parseInt(result.count)
 
         app.db('users')
@@ -15,12 +15,15 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
-    const getVeganRestaurants = (req, res) => {
+    const getVeganRestaurants = async (req, res) => {
+        const result = await app.db('users').where({ is_restaurant: true, vegan: true }).count('id').first()
+        const count = parseInt(result.count)
+
         app.db('users')
             .select('id', 'name', 'description')
             .where({ is_restaurant: true, vegan: true })
             .limit(limit)
-            .then(restaurants => res.json({ data: restaurants }))
+            .then(restaurants => res.json({ data: restaurants, count, limit }))
             .catch(err => res.status(500).send(err))
     }
 
