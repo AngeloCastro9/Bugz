906e7bfc5262e6247807ffcc11c6be4c868be0c1
angelocastro9 <castromatheus060@gmail.com>
2020-03-09 16:43:30 -0300

    Evol

--- frontend/src/components/auth/Auth.vue
+++ frontend/src/components/auth/Auth.vue
@@ -5,20 +5,15 @@
       <hr />
       <div class="auth-title">{{ showSignup ? 'Cadastro' : 'Login' }}</div>
 
-      <input v-if="showSignup" v-model="user.name" type="text" placeholder="Nome" required/>
+      <input v-if="showSignup" v-model="user.name" type="text" placeholder="Nome"/>
       <input v-if="showSignup" v-model="user.description" type="text" placeholder="Descrição" />
       <input v-if="showSignup" v-model="user.street" type="text" placeholder="Rua" />
       <input v-if="showSignup" v-model="user.number" type="text" placeholder="Número" />
       <input v-if="showSignup" v-model="user.neighborhood" type="text" placeholder="Bairro" />
-      <input v-if="showSignup" v-model="user.cpf_cnpj  " type="text" placeholder="cpf/cnpj" />
+      <input v-if="showSignup" v-model="user.cpf_cnpj  " type="text" placeholder="cpf/cnpj"/>
       <input v-model="user.email" name="email" type="text" placeholder="E-mail" />
       <input v-model="user.password" name="password" type="password" placeholder="Senha" />
-      <input
-        v-if="showSignup"
-        v-model="user.confirmPassword"
-        type="password"
-        placeholder="Confirme a Senha"
-      />
+      <input v-if="showSignup" v-model="user.confirmPassword" type="password" placeholder="Confirme a Senha"/>
 
       <div v-if="showSignup">
         <b-form-checkbox 
