ca5f5718550ad2c63ddad85195977b3fab157623
edivandosevero05 <edivandosevero05@gmail.com>
2020-04-25 09:16:13 -0300

    rota de teste

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const consign = require('consign')
 const db = require('./config/db')
 const cors = require('cors');
 
-app.db = db
+//app.db = db
 app.use(cors())
-consign()
-    .include('./config/passport.js')
-    .then('./config/middlewares.js')
-    .then('./api/validation.js')
-    .then('./api')
-    .then('./config/routes.js')
-    .into(app)
+// consign()
+//     .include('./config/passport.js')
+//     .then('./config/middlewares.js')
+//     .then('./api/validation.js')
+//     .then('./api')
+//     .then('./config/routes.js')
+//     .into(app)
 app.get('/teste', (req, res) => {
     return res.send('testado');
 })
