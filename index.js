13a9a4257d5b55f5a6a2cdc26606c97907724285
edivandosevero05 <edivandosevero05@gmail.com>
2020-04-26 08:50:18 -0300

    rota de teste

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const app = require('express')()
-//const consign = require('consign')
-//const db = require('./config/db')
+
 const cors = require('cors');
 
-//app.db = db
 app.use(cors())
-// consign()
-//     .include('./config/passport.js')
-//     .then('./config/middlewares.js')
-//     .then('./api/validation.js')
-//     .then('./api')
-//     .then('./config/routes.js')
-//     .into(app)
+
 app.get('/teste', (req, res) => {
     return res.send('testado');
 })
