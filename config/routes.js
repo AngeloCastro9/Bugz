c57278c06f27148acb0de7de070be7792f4218b8
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-05-01 21:34:00 -0300

    Teste de autorização

--- config/routes.js
+++ config/routes.js
@@ -27,7 +27,7 @@ module.exports = app => {
 
     // Cuidado com ordem! Tem que vir antes de /categories/:id
     app.route('/categories/tree')
-        .all(app.config.passport.authenticate())
+        // .all(app.config.passport.authenticate())
         .get(app.api.category.getTree)
 
     app.route('/categories/:id')
