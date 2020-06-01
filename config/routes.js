a2dee035d3f01580a48e0dec7997eed0b6970ebe
Yuri-Ribeiro <barrosribeiroyuri@gmail.com>
2020-06-01 20:09:50 -0300

    Teste 2

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,8 @@ module.exports = app => {
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signin)
     
-    app.post('/signupRestaurant/:id', app.api.restaurant.save)
+    app.post('/signupRestaurant', app.api.restaurant.save)
+    app.post('/updateRestaurant', app.api.restaurant.update)
     app.post('/signinRestaurant', app.api.authRestaurant.signin)
     
     app.post('/validateToken', app.api.auth.validateToken)
